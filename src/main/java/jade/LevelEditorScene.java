dc44acb9d049269cf34de82f65ca2eca98a58633
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-08-09 17:20:14 -0700

    ✨ Dirty Flags

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 
 public class LevelEditorScene extends Scene {
 
+    private GameObject obj1;
+    private SpriteSheet sprites;
 
     public LevelEditorScene() {
 
@@ -21,9 +23,9 @@ public class LevelEditorScene extends Scene {
 
         this.camera = new Camera(new Vector2f(-250, 0));
 
-        SpriteSheet sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
+        sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
 
-        GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+        obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
         obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
 
@@ -33,9 +35,24 @@ public class LevelEditorScene extends Scene {
 
     }
 
+    private int spriteIndex = 0;
+    private final float spriteFlipTime = 0.2f;
+    private float spriteFlipTimeLeft = 0.0f;
+
     @Override
     public void update(float dt) {
-//        System.out.println("Fps: " + (1.0f / dt));
+        spriteFlipTimeLeft -= dt;
+
+        if (spriteFlipTimeLeft <= 0) {
+            spriteFlipTimeLeft = spriteFlipTime;
+            spriteIndex++;
+
+            if (spriteIndex > 4) {
+                spriteIndex = 0;
+            }
+
+            obj1.getComponent(SpriteRenderer.class).setSprite(sprites.getSprite(spriteIndex));
+        }
 
         for (GameObject go : this.gameObjects) {
             go.update(dt);
